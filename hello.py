1694a30dfce07a2ebd097daf6d2edc8aa002f431
avikviolin <avikviolin@gmail.com>
2021-09-30 13:41:59 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,21 @@
-print("hello")
+def add(x,y):
+    return x+y
+
+def larger(x,y):
+    if (x>y):
+        return x
+    else:
+        return y
+
+def xor(a,b):
+    return ((a and not b) or (b and not a))
+
+def hello(n):
+    for i in range(n):
+        print("hello")
+
+def fraction(n):
+    for i in range(1,n+1):
+        print(float(1/i))
+    
+fraction(5)
